47eb4b6a012c73a0a8e5714905e6b11cf3b77a3d
dearguru07 <pangalaguruyadav101@gmail.com>
2025-10-07 22:12:26 +0530

    f

--- coding.py
+++ coding.py
@@ -1,53 +1,55 @@
-vowel = ['a', 'e', 'i', 'o', 'u']
-word = "programming"
-count = 0
-for character in word:
-    if character in vowel:
-        count += 1
-print(count)
+# vowel = ['a', 'e', 'i', 'o', 'u']
+# word = str(input('enter a str'))
+# count = 0
+# for character in word:
+#     if character in vowel:
+#         count += 1
+# print(count)
 
 
-vowel = ['a', 'e', 'i', 'o', 'u']
-word = "programming"
-count = 0
-for character in word:
-    if character not in vowel:
-        count += 1
-print(count)
+# vowel = ['a', 'e', 'i', 'o', 'u']
+# word = str(input('enter a str'))
+# count = 0
+# for character in word:
+#     if character not in vowel:
+#         count += 1
+# print(count)
 
 
 # max number finding-------------
 
-numberList = [15, 85, 35, 89, 125]
+# numberList = [15, 85, 35, 89, 125]
 
-maxNum = numberList[0]
-for num in numberList:
-    if maxNum < num:
-        maxNum = num
-print(maxNum)
+# maxNum = numberList[0]
+# for num in numberList:
+#     if maxNum < num:
+#         maxNum = num
+# print(maxNum)
 
 
 # min number finding------------
 
-numberList = [15, 85, 35, 89, 125, 2]
+# numberList = [15, 85, 35, 89, 125, 2]
 
-minNum = numberList[0]
-for num in numberList:
-    if minNum > num:
-        minNum = num
-print(minNum)
+# minNum = numberList[0]
+# for num in numberList:
+#     if minNum > num:
+#         minNum = num
+# print(minNum)
 
-# fing mid ele in list------------
 
-numList = [1, 2, 3, 4, 5]
-midElement = int((len(numList)/2)) 
+# # fing mid ele in list------------
 
-print(numList[midElement])
+# numList = [1, 2, 3, 4, 5]
+# midElement = int((len(numList)/2)) 
 
-# Anagrams or not-------------------
+# print(numList[midElement])
 
-str1 = "Listen"
-str2 = "Silent"
+
+# # Anagrams or not-------------------
+
+str1 = str(input('enter a str'))
+str2 = str(input('enter a str'))
 
 str1 = list(str1.upper())
 str2 = list(str2.upper())
@@ -58,9 +60,4 @@ if(str1 == str2):
 else:
     print("False")
 
-# pyramid partten-----------
 
-floors = 3
-h = 2*floors-1
-for i in range(1, 2*floors, 2):
-    print('{:^{}}'.format('*'*i, h))
--- index.py
+++ index.py
@@ -1,17 +1,43 @@
-# def Prime(n):
-#     tem=True
-#     er=n//2
-#     for i in range(2,er+1):
-#         if n%i==0:
-#             tem=False
-#             break
-#     if tem==True:
-#         print('prime')    
-#     else:
-#         print('not prime')    
-# n=int(input('enter a number'))        
-# Prime(n)
+def Prime(n):
+    tem=True
+    er=n//2
+    for i in range(2,er+1):
+        if n%i==0:
+            tem=False
+            break
+    if tem==True:
+        print('prime')    
+    else:
+        print('not prime')    
+n=int(input('enter a number'))        
+Prime(n)
 
+
+def Prime(n):
+    tem=True
+    er=n//2
+    for i in range(2,er+1):
+        if n%i==0:
+            tem=False
+            break
+    if tem==True:
+        print('prime')    
+    else:
+        print('not prime')    
+n=int(input('enter a number'))        
+Prime(n)def Prime(n):
+    tem=True
+    er=n//2
+    for i in range(2,er+1):
+        if n%i==0:
+            tem=False
+            break
+    if tem==True:
+        print('prime')    
+    else:
+        print('not prime')    
+n=int(input('enter a number'))        
+Prime(n)vv
 
 # def Count(n):
 #     countD=0
@@ -123,4 +149,70 @@ print(Reversed)
 if n==Reversed:
     print('polyndrome')
 else:
-    print('not a polyndrm')    
+    print('not a polyndrm')
+    
+
+# vowel = ['a', 'e', 'i', 'o', 'u']
+# word = str(input('enter a str'))
+# count = 0
+# for character in word:
+#     if character in vowel:
+#         count += 1
+# print(count)
+
+
+# vowel = ['a', 'e', 'i', 'o', 'u']
+# word = str(input('enter a str'))
+# count = 0
+# for character in word:
+#     if character not in vowel:
+#         count += 1
+# print(count)
+
+
+# max number finding-------------
+
+# numberList = [15, 85, 35, 89, 125]
+
+# maxNum = numberList[0]
+# for num in numberList:
+#     if maxNum < num:
+#         maxNum = num
+# print(maxNum)
+
+
+# min number finding------------
+
+# numberList = [15, 85, 35, 89, 125, 2]
+
+# minNum = numberList[0]
+# for num in numberList:
+#     if minNum > num:
+#         minNum = num
+# print(minNum)
+
+
+# # fing mid ele in list------------
+
+# numList = [1, 2, 3, 4, 5]
+# midElement = int((len(numList)/2)) 
+
+# print(numList[midElement])
+
+
+# # Anagrams or not-------------------
+
+str1 = str(input('enter a str'))
+str2 = str(input('enter a str'))
+
+str1 = list(str1.upper())
+str2 = list(str2.upper())
+str1.sort(), str2.sort()
+
+if(str1 == str2):
+    print("True")
+else:
+    print("False")
+
+
+    
